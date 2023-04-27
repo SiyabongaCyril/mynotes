84a7e5ace56c9dd86c622c16733518e8ce380b2a
Siyabonga Ntshingila <siya3.cyril@gmail.com>
2023-04-27 05:26:59 +0200

    Added Registration Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../firebase_options.dart';
 
 void main() {
+  //FutureBuilder requires the binding's BuildContext to function Properly
+  WidgetsFlutterBinding.ensureInitialized;
+  //Passing Material App Directly helps reduce our build times through hot reloads
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
-      primarySwatch: Colors.blue,
+      primarySwatch: Colors.deepPurple,
     ),
-    home: const HomePage(),
+    home: const RegisterView(),
   ));
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Account registration"),
+      ),
+      body: FutureBuilder(
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return Column(children: [
+                TextField(
+                  controller: _email,
+                  decoration:
+                      const InputDecoration(hintText: 'Enter your email'),
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                TextField(
+                  controller: _password,
+                  decoration:
+                      const InputDecoration(hintText: "Enter your password"),
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                ),
+                TextButton(
+                  onPressed: () async {
+                    final email = _email.text;
+                    final password = _password.text;
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                  },
+                  child: const Text("Register"),
+                )
+              ]);
+            default:
+              return const Text("Loading");
+          }
+        },
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+      ),
+      backgroundColor: Color.alphaBlend(Colors.grey, Colors.black),
+    );
   }
 }
