a1a9fa69583b5945902d035bd51a0a15b256b42c
Siyabonga Ntshingila <siya3.cyril@gmail.com>
2023-04-27 16:26:24 +0200

    added login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:mynotes/views/login_view.dart';
 import '../firebase_options.dart';
 
 void main() {
@@ -12,81 +13,6 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.deepPurple,
     ),
-    home: const RegisterView(),
+    home: const LoginView(),
   ));
-}
-
-class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
-
-  @override
-  State<RegisterView> createState() => _RegisterViewState();
-}
-
-class _RegisterViewState extends State<RegisterView> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Account registration"),
-      ),
-      body: FutureBuilder(
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Column(children: [
-                TextField(
-                  controller: _email,
-                  decoration:
-                      const InputDecoration(hintText: 'Enter your email'),
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                TextField(
-                  controller: _password,
-                  decoration:
-                      const InputDecoration(hintText: "Enter your password"),
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                ),
-                TextButton(
-                  onPressed: () async {
-                    final email = _email.text;
-                    final password = _password.text;
-                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                        email: email, password: password);
-                  },
-                  child: const Text("Register"),
-                )
-              ]);
-            default:
-              return const Text("Loading");
-          }
-        },
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-      ),
-      backgroundColor: Color.alphaBlend(Colors.grey, Colors.black),
-    );
-  }
 }
