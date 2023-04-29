068b9490d1b5e17451e677e3a100873ed8fefd00
Siyabonga Ntshingila <siya3.cyril@gmail.com>
2023-04-29 11:16:02 +0200

    Added Navigation between registration and login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
-import '../firebase_options.dart';
+import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 void main() {
   //FutureBuilder requires the binding's BuildContext to function Properly
@@ -13,6 +15,40 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.deepPurple,
     ),
-    home: const LoginView(),
+    home: const HomePage(),
+    routes: {
+      '/register/': (context) => const RegisterView(),
+      '/login/': (context) => const LoginView()
+    },
   ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              //  print('user: $user')
+              if (user != null) {
+                if (user.emailVerified) {
+                  return const Text('DONE');
+                } else {
+                  return const VerifyEmailView();
+                }
+              } else {
+                return const LoginView();
+              }
+
+            default:
+              return const CircularProgressIndicator();
+          }
+        });
+  }
 }
